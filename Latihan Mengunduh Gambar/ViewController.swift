90c550d7ec61660b336ba1ba95aab5b0d38a53ea
Erlangga Anugrah Arifin <angga1433@gmail.com>
2022-09-14 10:09:16 +0700

    Initial Commit

---
+++ Latihan Mengunduh Gambar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Latihan Mengunduh Gambar
+//
+//  Created by Erlangga Anugrah Arifin on 14/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
