8f05dfb47523773e98bd7d6b3ad8f2eeeaaa3d57
Erlangga Anugrah Arifin <angga1433@gmail.com>
2022-09-18 19:38:46 +0700

    Versi 1

--- Latihan Mengunduh Gambar/Controller/ViewController.swift
+++ Latihan Mengunduh Gambar/Controller/ViewController.swift
@@ -11,111 +11,140 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var movieTableView: UITableView!
-    private var movies: [Movie] = []
-    
-    // digunakan sebagai wadah dari antrean operation
-    private let pendingOperations = PendingOperations()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        movieTableView.dataSource = self
-        movieTableView.register(UINib(nibName: "MovieTableViewCell", bundle: nil), forCellReuseIdentifier: "movieTableViewCell") // MARK: movieTableViewCell adalah identifier dari file XIB MovieTableViewCell
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        Task { await getMovies() }
-      }
-     
-      func getMovies() async {
-        let network = NetworkService()
-        do {
-          movies = try await network.getMovies()
-          movieTableView.reloadData()
-        } catch {
-          fatalError("Error: connection failed.")
-        }
-      }
+  @IBOutlet var movieTableView: UITableView!
+  var movieTitle = String()
+  var overview = String()
+  var poster = String()
+  var elementName = String()
+  var id = 0
+  var movies = [Movie]()
 
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    movieTableView.dataSource = self
+
+    movieTableView.register(
+      UINib(nibName: "MovieTableViewCell", bundle: nil),
+      forCellReuseIdentifier: "movieTableViewCell"
+    )
+
+    if let path = Bundle.main.url(forResource: "Movies", withExtension: "xml") {
+      if let parser = XMLParser(contentsOf: path) {
+        parser.delegate = self
+        parser.parse()
+      }
+    }
+  }
 }
 
-
-// MARK: func tableView auto generate ketika movieTableView.dataSource = self diresolve oleh Xcode
 extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "movieTableViewCell", for: indexPath) as? MovieTableViewCell {
-            // file MovieModel sudah otomatis terdeteksi apabila memasukannya kedalam UITableViewDataSource
-            let movie = movies[indexPath.row]
-            cell.movieTitle.text = movie.title
-            cell.movieImage.image = movie.image
-//            cell.indicatorLoading.startAnimating()
-            
-            if movie.state == .new {
-              cell.indicatorLoading.isHidden = false
-              cell.indicatorLoading.startAnimating()
-              startOperations(movie: movie, indexPath: indexPath)
-            } else {
-              cell.indicatorLoading.stopAnimating()
-              cell.indicatorLoading.isHidden = true
-            }
-            return cell
-        } else {
-            return UITableViewCell()
-        }
-    }
-    
-    
-    //  Jika state dalam movie nilainya .new, kode memanggil fungsi startOperations
-    fileprivate func startOperations(movie: Movie, indexPath: IndexPath) {
+
+  func tableView(
+    _ tableView: UITableView,
+    numberOfRowsInSection section: Int
+  ) -> Int {
+    return movies.count
+  }
+
+  func tableView(
+    _ tableView: UITableView,
+    cellForRowAt indexPath: IndexPath
+  ) -> UITableViewCell {
+    if let cell = tableView.dequeueReusableCell(
+      withIdentifier: "movieTableViewCell",
+      for: indexPath
+    ) as? MovieTableViewCell {
+      
+      let movie = movies[indexPath.row]
+      cell.movieTitle.text = movie.title
+      cell.movieImage.image = movie.image
+
       if movie.state == .new {
+        cell.indicatorLoading.isHidden = false
+        cell.indicatorLoading.startAnimating()
         startDownload(movie: movie, indexPath: indexPath)
+      } else {
+        cell.indicatorLoading.stopAnimating()
+        cell.indicatorLoading.isHidden = true
+      }
+
+      return cell
+    } else {
+      return UITableViewCell()
+    }
+  }
+
+  fileprivate func startDownload(movie: Movie, indexPath: IndexPath) {
+    let imageDownloader = ImageDownloader()
+    if movie.state == .new {
+      Task {
+        do {
+          let image = try await imageDownloader.downloadImage(url: movie.poster)
+          movie.state = .downloaded
+          movie.image = image
+          self.movieTableView.reloadRows(at: [indexPath], with: .automatic)
+        } catch {
+          movie.state = .failed
+          movie.image = nil
+        }
       }
     }
-    
-    
-    // Ketika kondisi terpenuhi, kode melakukan pemanggilan function startDownload
-    fileprivate func startDownload(movie: Movie, indexPath: IndexPath) {
-        let imageDownloader = ImageDownloader()
-        if movie.state == .new {
-            // Task digunakan untuk menjalankan proses asynchronous dalam function synchronous. Ketika ingin menjalankan proses asynchronous secara paksa, terjadilah error
-            Task {
-                // Gunakan try-catch ketika memanggil imageDownloader karena memicu kesalahan. Ketika proses mengunduh gagal, kode masuk di bagian blok catch
-                do {
-                    let image = try await imageDownloader.downloadImage(url: movie.posterPath)
-                    movie.state = .downloaded
-                    movie.image = image
-                    self.movieTableView.reloadRows(at: [indexPath], with: .automatic)
-                } catch {
-                    movie.state = .failed
-                    movie.image = nil
-                }
-            }
-        }
-    }
+  }
 
 }
 
+extension ViewController: XMLParserDelegate {
+  func parser(
+    _ parser: XMLParser,
+    didStartElement elementName: String,
+    namespaceURI: String?,
+    qualifiedName qName: String?,
+    attributes attributeDict: [String: String] = [:]
+  ) {
+    if elementName == "movie" {
+      movieTitle = String()
+      overview = String()
+      poster = String()
 
-// MARK: menghentikan proses atau suspend saat pengguna melakukan scrolling dalam TableView
-extension ViewController: UIScrollViewDelegate {
-    // Ketika drag di UITableView tidak lag dengan tunda pengunduhan gambar
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        toggleSuspendOperations(isSuspended: true)
+      if let id = attributeDict["id"], let intId = Int(id) {
+        self.id = intId
+      }
     }
-    
-    // Ketika scroll user berhenti, lanjutkan pengunduhan
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        toggleSuspendOperations(isSuspended: false)
+
+    self.elementName = elementName
+  }
+
+  func parser(
+    _ parser: XMLParser,
+    didEndElement elementName: String,
+    namespaceURI: String?,
+    qualifiedName qName: String?
+  ) {
+    if elementName == "movie" {
+
+      let posterPath = URL(string: "https://image.tmdb.org/t/p/w300\(poster)")!
+      let movie = Movie(id: id, title: movieTitle, overview: overview, poster: posterPath)
+      movies.append(movie)
     }
-    
-    fileprivate func toggleSuspendOperations(isSuspended: Bool) {
-        pendingOperations.downloadQueue.isSuspended = isSuspended
+  }
+
+  func parser(
+    _ parser: XMLParser,
+    foundCharacters string: String
+  ) {
+    let data = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+
+    if !data.isEmpty {
+      switch elementName {
+      case "title": movieTitle = data
+      case "overview": overview = data
+      case "poster": poster = data
+      default: break
+      }
     }
-    
+  }
+
+  func parserDidEndDocument(_ parser: XMLParser) {
+    movieTableView.reloadData()
+  }
 }
--- Latihan Mengunduh Gambar/Model/MovieModel.swift
+++ Latihan Mengunduh Gambar/Model/MovieModel.swift
@@ -13,33 +13,20 @@ import UIKit
 // MARK: sebagai data model untuk menampung informasi movie
 // Konstanta title dan poster akan ditetapkan saat model Movie diinisialisasi, sedangkan variabel image secara default berupa variabel opsional dan untuk state memiliki nilai default berupa .new
 class Movie {
-  let title: String
-  let popularity: Double
-  let genres: [Int]
-  let voteAverage: Double
+  let id: Int
   let overview: String
-  let releaseDate: Date
-  let posterPath: URL
- 
+
+  let title: String
+  let poster: URL
+
   var image: UIImage?
   var state: DownloadState = .new
- 
-  init(
-    title: String,
-    popularity: Double,
-    genres: [Int],
-    voteAverage: Double,
-    overview: String,
-    releaseDate: Date,
-    posterPath: URL
-  ) {
+
+  init(id: Int, title: String, overview: String, poster: URL) {
+    self.id = id
     self.title = title
-    self.popularity = popularity
-    self.genres = genres
-    self.voteAverage = voteAverage
     self.overview = overview
-    self.releaseDate = releaseDate
-    self.posterPath = posterPath
+    self.poster = poster
   }
 }
 
@@ -52,60 +39,3 @@ enum DownloadState {
 //failed: merupakan state yang menunjukkan bahwa proses mengunduh gagal dilakukan.
 }
 
-
-// MARK: menampung data sementara dari API
-struct MovieResponses: Codable {
-  let page: Int
-  let totalResults: Int
-  let totalPages: Int
-  let movies: [MovieResponse]
- 
-  enum CodingKeys: String, CodingKey {
-    case page
-    case totalResults = "total_results"
-    case totalPages = "total_pages"
-    case movies = "results"
-  }
-}
- 
-struct MovieResponse: Codable {
-  let popularity: Double
-  let title: String
-  let genres: [Int]
-  let voteAverage: Double
-  let overview: String
-  let releaseDate: Date
- 
-  let posterPath: URL
- 
-  enum CodingKeys: String, CodingKey {
-    case popularity
-    case posterPath = "poster_path"
-    case title
-    case genres = "genre_ids"
-    case voteAverage = "vote_average"
-    case overview
-    case releaseDate = "release_date"
-  }
- 
-  init(from decoder: Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
- 
-    // Menentukan alamat gambar, dan menyesuaikan posterPath menjadi URL agar lebih mudah untuk digunakan
-    let path = try container.decode(String.self, forKey: .posterPath)
-    posterPath = URL(string: "https://image.tmdb.org/t/p/w300\(path)")!
- 
-    // Menentukan tanggal rilis
-    let dateString = try container.decode(String.self, forKey: .releaseDate)
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy-MM-dd"
-    releaseDate = dateFormatter.date(from: dateString)!
- 
-    // Untuk properti lainnya, cukup disesuaikan saja.
-    popularity = try container.decode(Double.self, forKey: .popularity)
-    title = try container.decode(String.self, forKey: .title)
-    genres = try container.decode([Int].self, forKey: .genres)
-    voteAverage = try container.decode(Double.self, forKey: .voteAverage)
-    overview = try container.decode(String.self, forKey: .overview)
-  }
-}
